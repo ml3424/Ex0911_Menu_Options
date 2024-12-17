fc8c5df5b619d9cf6ee3e3de079e8dd2862bc3f4
ml3424 <ml3424@bs.amalnet.k12.il>
2024-12-17 19:54:49 +0200

    added onOptionsItemSelected

--- app/src/main/java/com/example/ex0911_menu_options/MainActivity.java
+++ app/src/main/java/com/example/ex0911_menu_options/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.ex0911_menu_options;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +14,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    TextView tV_result;
+    EditText eT1, eT2;
 
     String inputET1_str = "", inputET2_str = "";
     int inputET1 = 0, inputET2 = 0;
@@ -20,7 +23,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tV_result = findViewById(R.id.tV_result);
+        eT1 = findViewById(R.id.eT1);
+        eT2 = findViewById(R.id.eT2);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu (Menu menu) {
@@ -32,10 +40,30 @@ public class MainActivity extends AppCompatActivity {
 
     public boolean onOptionsItemSelected (MenuItem item) {
         int id = item.getItemId();
+
         if (id == R.id.menuPlus) {
-            tv4.setText("" + (inputET1 + inputET2));
+            tV_result.setText("%.3f" + (inputET1 + inputET2));
             return true;
         }
+        else if (id == R.id.menuMinus) {
+            tV_result.setText("%.3f" + (inputET1 - inputET2));
+            return true;
+        }
+        else if (id == R.id.menuMul) {
+            tV_result.setText("%.3f" + (inputET1 * inputET2));
+            return true;
+        }
+        else if (id == R.id.menuDiv) {
+            if(inputET2 == 0)
+            {
+                tV_result.setText("Error: Division by zero!");
+                return false;
+            }
+            tV_result.setText("%.3f" + (inputET1 / inputET2));
+            return true;
+        }
+
+        return false;
     }
 
 }
